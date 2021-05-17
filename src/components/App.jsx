22646ababb472ed8e6a42e19c45e25676d6dc0e6
Utk0201 <utkarsh18e@iitg.ac.in>
2021-05-17 11:28:46 +0530

    changed counter using react states

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
 
 function App() {
-  return <div />;
+  var [ct, setCt] = useState(0);
+  return (
+    <div className="container">
+      <h1>{ct}</h1>
+      <button onClick={() => setCt(ct - 1)}>-</button>
+      <button onClick={() => setCt(ct + 1)}>+</button>
+    </div>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,25 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./components/App.jsx";
 
 //  we must use hook inside a functional component
 
-var ct = 0;
-
-function clk() {
-  ct++;
-  ReactDOM.render(
-    <div className="container">
-      <h1>{ct}</h1>
-      <button onClick={clk}>+</button>
-    </div>,
-    document.getElementById("root")
-  );
-}
-
-ReactDOM.render(
-  <div className="container">
-    <h1>{ct}</h1>
-    <button onClick={clk}>+</button>
-  </div>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
